5579852f899515765e9c2c3e000803ff41dda6c3
Tester <majiatester@gmail.com>
2016-10-08 16:46:30 -0400

    Added Top N Freuqent Words in Menu
    exclude "with" "and"...

--- src/MenuProcess/Process.java
+++ src/MenuProcess/Process.java
@@ -1,32 +1,39 @@
 package MenuProcess;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-
-
-
 
 public class Process {
 
 	private static final int GOOD = 5;
 	private static final int NEUTRUAL = 3;
 	private static final int BAD = 1;
+	private static final int HIGHCAL = 400; // TODO: customize from user input
+	private static final int NUM_KEYWORD = 5;// pick first 5 frequent words
+
 	
 	static HashSet<Sample> tempMenu;//temporarily holds menu under processing. 
 	//forget why use static instead of passing as argument...
 	static HashMap<String, SingleMenuReport> reportCollection; //<store ID, menu items>, all single reports here
 	
-	//For single menu report. more criteria to add.
-	public static  int rate(HashSet<Sample> menu) 
+	static HashMap <String, Integer> keywordCount;//keep count of each word's occurrence in item names
+	
+	//For single menu report. Rate healthiness based on low cal food
+	private static  int rate(HashSet<Sample> menu) 
 	{
 		int highCals  =0  , lowCals = 0;
 		
 		for (Sample s: menu)
 		{
-			if (s.calories > 400)
+			if (s.calories > HIGHCAL)
 				highCals++;
 			else
 				lowCals++;
@@ -41,20 +48,66 @@ public class Process {
 		
 	}
 	
+	//For single menu report, get item name word count 
+	//ie: Chicken Wing, Chicken Breast, count result <Chicken, 2>, <Wing, 1> ,<Breast,1>
+	private static void countKeyWord(HashMap <String, Integer> count)
+	{
+		String name;
+		String delims = "[ .,?!]+";
+		String[] tokens;
+		
+		for (Sample s:tempMenu)
+		{
+			name = s.name;	
+			tokens= name.split(delims); // break down item name into words
+
+			for (String t: tokens)
+			{
+				if(t.equals("with") == false && t.equals("and") == false)// exclude common words
+				{
+					if (count.containsKey(t))
+						count.replace(t, count.get(t), (count.get(t) + 1));
+					else
+						count.put(t, 1);
+				}
+			}
+		}		
+	}
+	
 	
 	public static void main(String args[]) throws JsonSyntaxException, IOException
 	{
 		tempMenu = new HashSet<Sample>();
 		reportCollection = new HashMap<String, SingleMenuReport>();
+		SingleMenuReport report;
 		Gson gson;
 		String jsonStr;
+		
+		int rating;
+		keywordCount  = new HashMap <String, Integer>();
 		
 		//Process Json in each file. 
 		for (String s: args)
 		{
 			System.out.println("\n\n" + s);
-			UnWrap.json2Java(s);
-			reportCollection.put(s, new SingleMenuReport(s,rate(tempMenu)));
+			UnWrap.json2Java(s);//Json to java
+			rating = rate(tempMenu);//get rating
+			countKeyWord(keywordCount);//get word count
+			
+			//Sort key words by count
+			 ValueComparator bvc =  new ValueComparator(keywordCount);
+			 TreeMap<String,Integer> sortedWordCount = new TreeMap<String,Integer>(bvc);
+			 sortedWordCount.putAll(keywordCount);			 
+			
+			report = new SingleMenuReport(s,rating);
+			//Add top words to report
+			Iterator iter = sortedWordCount.entrySet().iterator();
+			for (int i =0; i< NUM_KEYWORD; i++)
+			{	Map.Entry<String, Integer> entry = (Entry<String, Integer>) iter.next();
+				report.addTopWord(entry.getKey(), entry.getValue());
+			}
+			
+			reportCollection.put(s, report);
 			System.out.println("Rating is: " + rate(tempMenu));
 			
 			tempMenu.clear();
@@ -62,10 +115,31 @@ public class Process {
 		
 		//Wrap all single reports into Json
 		gson = new Gson();
-		jsonStr = gson.toJson(reportCollection); // 
-		
+		jsonStr = gson.toJson(reportCollection);
+		reportCollection.clear();
+		keywordCount.clear();
 		System.out.println("\n\n Json string: ");
 		System.out.println(jsonStr);
 
 	}
+}
+
+/*
+ *For sorting the most frequent (most count) words from HashMap of words and count /
+ */
+class ValueComparator implements Comparator<String> {
+
+    HashMap<String, Integer> base;
+    public ValueComparator(HashMap<String, Integer> base) {
+        this.base = base;
+    }
+
+    // Note: this comparator imposes orderings that are inconsistent with equals.    
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
 }
--- src/MenuProcess/SingleMenuReport.java
+++ src/MenuProcess/SingleMenuReport.java
@@ -1,19 +1,34 @@
 package MenuProcess;
 
+import java.util.HashMap;
+
 public class SingleMenuReport 
 {
 		
 		String storeID; //Unique store name as primary key
-		int rating;
-		
-		public SingleMenuReport()
-		{rating = 0;}
+		double lat, log;
+		int rating; //based on ratio of low cal food
+		HashMap<String, Integer> topWordCount;
 		
 		public SingleMenuReport(String ID, int r)
 		{
 			storeID = ID;
 			this.rating = r;
+			topWordCount = new HashMap<String, Integer>();
 		}
+		
+		public SingleMenuReport(String ID, int r, double lat , double log)
+		{
+			storeID = ID;
+			this.rating = r;
+			this.lat = lat;
+			this.log = log;
+			topWordCount = new HashMap<String, Integer>();
+		}
+		
+		public void addTopWord(String str, Integer in)
+		{topWordCount.put(str, in);}
+		
 
 }
 
